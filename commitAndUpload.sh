a96ea3e15a761213be5e3f395e4d24076f10cab0
James Lambert <james.a.lambert@gmail.com>
2021-10-05 17:18:19 +0100

    test commit

--- commitAndUpload.sh
+++ commitAndUpload.sh
@@ -1,3 +1,13 @@
+
+if [$1 -eq ""]
+then
+	echo "please write a commit message"
+	exit 1
+fi
+
+
 git add --all
+echo "commiting changes"
 git commit -m "$1"
+echo "pushing changes"
 git push -u origin master
