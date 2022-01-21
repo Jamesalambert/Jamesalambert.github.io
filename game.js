efe386078e6500f1fcb9048ec882bc0bca8fd0cb
James Lambert <james.a.lambert@gmail.com>
2022-01-21 18:06:49 +0000

    added reset button js

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var isCrossTurn = True;
+var isCrossTurn = true;
 
 
 function tappedSquare(buttonID){
@@ -6,9 +6,21 @@ function tappedSquare(buttonID){
 	var button = document.getElementById(buttonID);
 	if(isCrossTurn){
 		button.className = "square cross";
+        button.innerHTML = "x"
 	} else {
 		button.className = "square nought";
+        button.innerHTML = "o"
 	}
-	
 	isCrossTurn = !isCrossTurn;
+}
+
+function reset(){
+    //reset the buttons!
+    var buttons = document.getElementsByClassName("square");
+    
+    for(var i=0; i < buttons.length; i++){
+        buttons[i].innerHTML = "";
+        buttons[i].className = "square"
+    }
+    
 }
