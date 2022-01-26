f94ba8767695f7c7f4ae69acce71b1315f208a86
James Lambert <james.a.lambert@gmail.com>
2022-01-26 18:06:55 +0000

    tweaked css

--- game.js
+++ game.js
@@ -2,17 +2,24 @@ var isCrossTurn = true;
 
 
 function tappedSquare(buttonID){
-    
 	var button = document.getElementById(buttonID);
+
+// 	check to see if the button 
+	if(button.className == "square cross" || button.className == "square nought"){
+		console.log("tried to click on an occupied square!");
+		return;
+	}
+	
 	if(isCrossTurn){
 		button.className = "square cross";
-        button.innerHTML = "x"
+        button.innerHTML = "x";
 	} else {
 		button.className = "square nought";
-        button.innerHTML = "o"
+        button.innerHTML = "o";
 	}
 	isCrossTurn = !isCrossTurn;
 }
+
 
 function reset(){
     //reset the buttons!
@@ -20,7 +27,7 @@ function reset(){
     
     for(var i=0; i < buttons.length; i++){
         buttons[i].innerHTML = "";
-        buttons[i].className = "square"
+        buttons[i].className = "square";
     }
-    
+    isCrossTurn = true;
 }
