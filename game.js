42545d37d07e5e9a8afbb6aeb9ac08c28823a9cf
James Lambert <james.a.lambert@gmail.com>
2022-02-09 14:55:15 +0000

    adding code to detect a winner

--- game.js
+++ game.js
@@ -1,10 +1,16 @@
 var isCrossTurn = true;
 
+var WINNING_LINES = ['abc','def','ghi','adg','beh','cfi','aei','ceg'];
+
+var noughtsSquares = ""; //-> does this contain either "abc" or "def" ???
+var crossesSquares = "";  
+
 
 function tappedSquare(buttonID){
 	var button = document.getElementById(buttonID);
 
-// 	check to see if the button 
+// THIS SHOULD CHECK THE MODEL NOT THE UI
+// 	check to see if the button has been clicked before.
 	if(button.className == "square cross" || button.className == "square nought"){
 		console.log("tried to click on an occupied square!");
 		return;
@@ -13,21 +19,34 @@ function tappedSquare(buttonID){
 	if(isCrossTurn){
 		button.className = "square cross";
         button.innerHTML = "x";
+		crossesSquares += buttonID;
 	} else {
 		button.className = "square nought";
-        button.innerHTML = "o";
+    	button.innerHTML = "o";
+		noughtsSquares += buttonID;
 	}
+	console.log(noughtsSquares);
+	console.log(crossesSquares);
+// 	check to see if there is a winner
+	
+// if there is what should we do?????
+
 	isCrossTurn = !isCrossTurn;
 }
 
 
+
 function reset(){
-    //reset the buttons!
+    //reset the buttons
     var buttons = document.getElementsByClassName("square");
-    
     for(var i=0; i < buttons.length; i++){
         buttons[i].innerHTML = "";
         buttons[i].className = "square";
     }
+    //reset turns
     isCrossTurn = true;
+    
+    //reset memory
+    noughtsSquares = "";
+	crossesSquares = "";
 }
